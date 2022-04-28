5267155c68e58f90d1ffaa300adb336f2e2b2ac7
hardy lutula <hardylutula@gmail.com>
2022-04-28 10:15:15 +0200

    Basic setup + server added 😁

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Mern Authentitaction
+
+1. Rename the file "**env.txt**" to "**.env**", and replace all environment variables values 
---
+++ server/server.js
@@ -0,0 +1,12 @@
+
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 4000
+
+const app = express()
+
+app.get('/api', (req, res)=> {
+    res.status(200).json({message: 'The server is up'})
+})
+
+app.listen(PORT, () => console.log(`Server started on Port: ${PORT}`))
