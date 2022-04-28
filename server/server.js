de7c5b271a5e892b16009b2fbe2a8f6eb16405b6
hardy lutula <hardylutula@gmail.com>
2022-04-28 13:22:56 +0200

    body-parser + cors added

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 4000
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
