624fd0f6c2781b7cf36be4be4877d9ee85cd7df5
hardy lutula <hardylutula@gmail.com>
2022-04-29 21:44:46 +0200

    User register added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,7 @@
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 // @desc        Register user
 // @route       POST /api/users
@@ -7,7 +9,34 @@ const User = require('../models/userModel')
 // @url-param   -
 // @access      public
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({message: 'user registered', data: req.body})
+  const {name, email, password} = req.body
+
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error('All fields are required')
+  }
+
+  // check if user exits
+  const userExists = await User.findOne({email})
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  // hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // create user
+  const user = await User.create({name, email, password: hashedPassword})
+
+  if (user) {
+    res.status(201).json({_id: user.id, name: user.name, email: user.email})
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
 })
 
 // @desc        Authenticate user
