19ebb770672a0dedf25e3826e41a6ee1c84bccf7
hardy lutula <hardylutula@gmail.com>
2022-04-29 20:15:29 +0200

    User schema added, Goal schema edited so that it can reference the user

--- server/models/goalModel.js
+++ server/models/goalModel.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose')
 
 const schema = mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
     text: {
       type: String,
       require: [true, 'Please add a text value'],
