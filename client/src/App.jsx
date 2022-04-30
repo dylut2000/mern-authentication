3d68e16e552ed05cac3d39e08c5b170f8589ed8c
hardy lutula <hardylutula@gmail.com>
2022-04-30 13:24:29 +0200

    Header added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import {Routes, Route} from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
-      <div className="App">
+      <div className="app">
+        <Header />
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="login" element={<Login />} />
