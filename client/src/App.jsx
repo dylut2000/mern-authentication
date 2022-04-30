1e62ad2edda176ce1000400a3bd66a1bd93a1a26
hardy lutula <hardylutula@gmail.com>
2022-04-30 11:53:07 +0200

    basic routing added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
+import {Routes, Route} from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
 function App() {
   return (
-    <div className="App">
-      <h1>hardy lutula dylut2000</h1>
-    </div>
+    <>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Routes>
+      </div>
+    </>
   )
 }
 
