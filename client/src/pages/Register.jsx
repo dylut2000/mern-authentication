c0457c41ee2d82f71040577289fd2b51609b950c
hardy lutula <hardylutula@gmail.com>
2022-05-02 09:29:47 +0200

    Register page styled

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,33 @@
+import {useState} from 'react'
+import {FaUser} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+
 const Register = () => {
-  return <div>Register</div>
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+  const {name, email, password, confirmPassword} = formData
+
+  return (
+    <div className="auth">
+      <div className="auth__card">
+        <FaUser className="logo" />
+        <h2>Create an account</h2>
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <input type="password" placeholder="Confirm Password" />
+        <button className="btn btn-primary">Register</button>
+        <p>
+          Do you have an account? <Link to={'login'}>Login</Link>
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default Register
