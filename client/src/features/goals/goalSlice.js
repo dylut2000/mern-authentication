d182701d67831e521a3ee5ccd83610cecdae9316
hardy lutula <hardylutula@gmail.com>
2022-05-12 15:15:30 +0200

    getGoals added

--- client/src/features/goals/goalSlice.js
+++ client/src/features/goals/goalSlice.js
@@ -9,6 +9,7 @@ const initialState = {
   message: '',
 }
 
+// create goal
 export const createGoal = createAsyncThunk(
   'goals/create',
   async (goalData, thunkAPI) => {
@@ -16,6 +17,26 @@ export const createGoal = createAsyncThunk(
       const token = thunkAPI.getState().auth.user.token
 
       return await goalService.createGoal(goalData, token)
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+      return thunkAPI.rejectWithValue(message)
+    }
+  }
+)
+
+// get user goals
+export const getGoals = createAsyncThunk(
+  'goals/getAll',
+  async (_, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token
+
+      return await goalService.getGoals(token)
     } catch (error) {
       const message =
         (error.response &&
@@ -45,6 +66,19 @@ export const goalSlice = createSlice({
         state.goals.push(action.payload)
       })
       .addCase(createGoal.rejected, (state, action) => {
+        state.isLoading = false
+        state.isError = true
+        state.message = action.payload
+      })
+      .addCase(getGoals.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(getGoals.fulfilled, (state, action) => {
+        state.isLoading = false
+        state.isSuccess = true
+        state.goals = action.payload
+      })
+      .addCase(getGoals.rejected, (state, action) => {
         state.isLoading = false
         state.isError = true
         state.message = action.payload
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,17 +1,38 @@
 import {useEffect} from 'react'
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
 import {useNavigate} from 'react-router-dom'
 import GoalForm from '../components/GoalForm'
+import Spinner from '../components/Spinner'
+import {getGoals} from '../features/goals/goalSlice'
+import {reset} from '../features/goals/goalSlice'
 
 const Dashboard = () => {
   const navigate = useNavigate()
+  const dispatch = useDispatch()
   const {user} = useSelector((state) => state.auth)
+  const {goals, isLoading, isError, message} = useSelector(
+    (state) => state.goals
+  )
 
   useEffect(() => {
+    if (isError) {
+      console.log(message)
+    }
+
     if (!user) {
       navigate('/login')
     }
-  }, [user, navigate])
+
+    dispatch(getGoals())
+
+    return () => {
+      dispatch(reset())
+    }
+  }, [user, navigate, isError, message, dispatch])
+
+  if (isLoading) {
+    return <Spinner />
+  }
 
   return (
     <div className="dash">
