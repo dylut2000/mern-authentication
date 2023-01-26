e74b79ed19de6d3527459a98940173c586e41bcb
Hardy Lutula <hardylutula@gmail.com>
2023-01-26 23:10:45 +0200

    register added..

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -1,3 +1,6 @@
+import bcrypt from 'bcrypt'
+import UserModel from '../model/User.model.js'
+
 /**
  * register user
  * POST: http://localhost:8080/api/register
@@ -14,7 +17,42 @@
  */
 
 export async function register(req, res) {
-  res.json('register route')
+  try {
+    const {username, password, profile, email} = req.body
+
+    // check if username exist
+
+    const existUsername = await UserModel.findOne({username})
+
+    if (existUsername)
+      return res.status(500).json({msg: 'Username or email already exist'})
+
+    // check if email exist
+
+    const existEmail = await UserModel.findOne({email})
+
+    if (existEmail)
+      return res.status(400).json({msg: 'Username or email already exist'})
+
+    // hash password
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    // create user
+
+    const user = UserModel({
+      username,
+      password: hashedPassword,
+      profile: profile || '',
+      email,
+    })
+
+    await user.save()
+
+    return res.status(201).json({msg: 'successfull registered'})
+  } catch (error) {
+    res.status(500).send(error)
+  }
 }
 
 /**
